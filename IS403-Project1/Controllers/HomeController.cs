e8798554f66968535e1a7db7b2442773636310bd
McKay Dalling <mdalling94@gmail.com>
2019-11-18 17:31:58 -0700

    McKay added a comment to the HomeController

--- IS403-Project1/Controllers/HomeController.cs
+++ IS403-Project1/Controllers/HomeController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+// This is a really helpful comment that McKay wrote and pushed to github
 namespace IS403_Project1.Controllers
 {
     public class HomeController : Controller
