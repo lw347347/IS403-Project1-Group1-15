2f3af0014134147682adcfa49748f63c56557281
Landon Williams <lw347347@byu.edu>
2019-11-20 20:19:27 -0700

    Added project description and authors

--- IS403-Project1/Controllers/HomeController.cs
+++ IS403-Project1/Controllers/HomeController.cs
@@ -5,6 +5,12 @@ using System.Web;
 using System.Web.Mvc;
 // This is a really helpful comment that McKay wrote and pushed to github
 // Matt's comment. Slightly shorter and sexier than McKay's.
+
+// This website is for being able to swap surveys. 
+// You can take more surveys to make yours show up on others' pages.
+
+// Authors
+    // Mckay Dalling, Matthew Gardner, Parker Pixton, and Landon Williams
 namespace IS403_Project1.Controllers
 {
     public class HomeController : Controller
