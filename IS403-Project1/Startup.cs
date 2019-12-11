893c6b7d21b7fe988eb1aa47b5e36cf00a47aae4
Matthew Gardner <matt.gardner32@gmail.com>
2019-12-11 00:10:46 -0700

    tried some changes to get ready for FB authentication

---
+++ IS403-Project1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IS403_Project1.Startup))]
+namespace IS403_Project1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
